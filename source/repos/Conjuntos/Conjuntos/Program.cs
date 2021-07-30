19169bb8ea1f46a50d7e3edea15aee121748eca7
Rogerio <rveneroso2003@gmail.com>
2021-07-30 15:34:48 -0300

    Subindo meu segundo arquivo

---
+++ source/repos/Conjuntos/Conjuntos/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Conjuntos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> A = new HashSet<int>();
+            HashSet<int> B = new HashSet<int>();
+            HashSet<int> C = new HashSet<int>();
+            Console.Write("O curso A possui quantos alunos? ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Digite os códigos dos alunos do curso A: ");
+            string[] values = Console.ReadLine().Split(' ');
+            for (int i = 0; i < n; i++)
+            {
+                A.Add(int.Parse(values[i]));
+            }
+            Console.Write("O curso B possui quantos alunos? ");
+            n = int.Parse(Console.ReadLine());
+            Console.Write("Digite os códigos dos alunos do curso B: ");
+            values = Console.ReadLine().Split(' ');
+            for (int i = 0; i < n; i++)
+            {
+                B.Add(int.Parse(values[i]));
+            }
+            Console.Write("O curso C possui quantos alunos? ");
+            n = int.Parse(Console.ReadLine());
+            Console.Write("Digite os códigos dos alunos do curso C: ");
+            values = Console.ReadLine().Split(' ');
+            for (int i = 0; i < n; i++)
+            {
+                C.Add(int.Parse(values[i]));
+            }
+
+            A.UnionWith(B);
+            A.UnionWith(C);
+            int cont = 0;
+            foreach (int x in A)
+            {
+                cont++;
+            }
+            Console.WriteLine("Total de alunos:" + cont);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ source/repos/Matrizes/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mat = "";
+            var menor = 0;
+            int numsorteado = 0;
+            int[] numeros = new int[9];
+            int[,] array = new int[3, 5];
+            for (int i = 0; i <= 2; i++)
+            {
+                for (int y = 0; y <= 4; y++)
+                {
+                    Random randNum = new Random();
+                    randNum.Next();
+                    numsorteado = randNum.Next(8);
+                    numeros[numsorteado] = numeros[numsorteado] + 1;
+                    while (numeros[numsorteado] > 2)
+                    {
+                        numsorteado = randNum.Next(8);
+                        numeros[numsorteado] = numeros[numsorteado] + 1;
+                    }
+                    array[i, y] = numsorteado;
+                    if (numeros[numsorteado]==1)
+                    {
+                        menor = numsorteado;
+                    }
+                    mat += array[i, y] + "|";
+                    if (y == 4)
+                    {
+                        Console.WriteLine(mat);
+                        mat = "";
+                    }
+                }
+            }
+            Console.WriteLine("Número que menos vezes apareceu na matriz é: " + menor);
+            Console.ReadLine();
+        }
+    }
+}
